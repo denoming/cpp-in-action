1001c2459bf7ba478a90416c9c3c0e282d644633
Denys Asauliak <denoming@gmail.com>
2022-02-09 22:05:02 +0200

    ref cmake implementation

--- cmake/modules/BuildType.cmake
+++ cmake/modules/BuildType.cmake
@@ -1,8 +1,21 @@
-# Set a default build type if none was specified
-if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-  message(STATUS "Setting build type to 'Debug' as none was specified.")
-  set(CMAKE_BUILD_TYPE Debug CACHE STRING "Choose the type of build." FORCE)
-  # Set the possible values of build type for cmake-gui
-  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release"
-               "MinSizeRel" "RelWithDebInfo")
+get_property(isMultiConfig GLOBAL
+        PROPERTY GENERATOR_IS_MULTI_CONFIG
+        )
+if (isMultiConfig)
+  if (NOT "Profile" IN_LIST CMAKE_CONFIGURATION_TYPES)
+    list(APPEND CMAKE_CONFIGURATION_TYPES Profile)
+  endif()
+else()
+  set(allowedBuildTypes Debug Release Profile MinSizeRel RelWithDebInfo)
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY
+          STRINGS "${allowedBuildTypes}"
+          )
+  if (CMAKE_BUILD_TYPE)
+    if (NOT CMAKE_BUILD_TYPE IN_LIST allowedBuildTypes)
+      message(FATAL_ERROR "Unknown build type: ${CMAKE_BUILD_TYPE}")
+    endif()
+  else()
+    message(VERBOSE "Set build type to 'Debug' as none was specified")
+    set(CMAKE_BUILD_TYPE Debug CACHE STRING "" FORCE)
+  endif()
 endif()
