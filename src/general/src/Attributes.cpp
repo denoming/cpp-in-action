2a6dda1f549b3fac7981991b120e8f51b8ea5b2a
Denys Asauliak <denoming@gmail.com>
2023-01-07 17:26:28 +0200

    add additional attributes

--- src/general/src/Attributes.cpp
+++ src/general/src/Attributes.cpp
@@ -1,8 +1,15 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 #include <iostream>
 #include <string>
 #include <thread>
+#include <vector>
+#include <cmath>
+
+using namespace testing;
+
+//--------------------------------------------------------------------------------------------------
 
 class Data {
 public:
@@ -41,4 +48,84 @@ TEST(Attributes, NoDiscard)
     if (t.joinable()) {
         t.join();
     }
+}
+
+//--------------------------------------------------------------------------------------------------
+
+enum class [[nodiscard]] ErrorCode { Ok, Failed };
+
+ErrorCode
+errorProneFunction()
+{
+    return ErrorCode::Failed;
+}
+
+TEST(Attributes, NoDiscardWithEnum)
+{
+    // Warning: ignoring returned value of type ‘ErrorCode’
+    // errorProneFunction();
+
+    EXPECT_EQ(errorProneFunction(), ErrorCode::Failed);
+}
+
+//--------------------------------------------------------------------------------------------------
+
+struct MyType {
+    /* */
+    [[nodiscard("Implicit destroying of temporary object")]] MyType(int, bool)
+    {
+    }
+};
+
+TEST(Attributes, NoDiscardWithConstructor)
+{
+    // Warning: ignoring return value of ‘MyType::MyType(int, bool)’
+    // MyType{5, true};
+}
+
+//--------------------------------------------------------------------------------------------------
+
+TEST(Attributes, LikelyNoLikely)
+{
+    std::vector<double> numbers{-1.0, -3.0, -5.0, -7.0};
+
+    double sum{0};
+    for (double number : numbers) {
+        if (number < 0) [[likely]] { // Specify that this branch is more likely
+            sum -= std::sqrt(-number);
+        } else {
+            sum += std::sqrt(number);
+        }
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+
+struct Empty { };
+
+struct NoUniqueAddress {
+    int d{};
+    [[no_unique_address]] Empty e{}; // Do not allocate dedicated memory address fo class member
+};
+
+struct UniqueAddress {
+    int d{};
+    Empty e{};
+};
+
+TEST(Attributes, NoUniqueAddress)
+{
+    EXPECT_EQ(sizeof(int), sizeof(NoUniqueAddress));
+    EXPECT_NE(sizeof(int), sizeof(UniqueAddress));
+
+    NoUniqueAddress noUnique;
+    std::cout << "&NoUnique.d: " <<  &noUnique.d << '\n';
+    std::cout << "&NoUnique.e: " <<  &noUnique.e << '\n';
+
+    UniqueAddress unique;
+    std::cout << "&unique.d: " <<  &unique.d << '\n';
+    std::cout << "&unique.e: " <<  &unique.e << '\n';
+
+    EXPECT_EQ(static_cast<void*>(&noUnique.d), &noUnique.e);
+    EXPECT_NE(static_cast<void*>(&unique.d), &unique.e);
 }
