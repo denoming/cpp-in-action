a20491128b5b5cda92d3c951f99ad97ed55bf7aa
Denys Asauliak <denoming@gmail.com>
2023-02-11 12:58:16 +0200

    add async resolve example

--- src/network/src/Resolver.cpp
+++ src/network/src/Resolver.cpp
@@ -1,23 +1,47 @@
-#include <boost/asio.hpp>
-
 #include <gtest/gtest.h>
+
+#include <boost/asio.hpp>
 
 #include <iostream>
 
-using namespace boost;
+namespace asio = boost::asio;
+namespace sys = boost::system;
+
+using tcp = asio::ip::tcp;
+
+static std::string_view Host{"www.google.com"};
+static std::string_view Port{"80"};
 
 TEST(Resolver, Resolve)
 {
-    static std::string_view Host{"www.google.com"};
-    static std::string_view Port{"80"};
-
-    asio::io_context ioc;
-    asio::ip::tcp::resolver resolver{ioc};
-    system::error_code ec;
-    const auto results = resolver.resolve( Host, Port, ec);
-    if (!ec) {
+    asio::io_context context;
+    asio::ip::tcp::resolver resolver{context};
+    sys::error_code error;
+    const auto results = resolver.resolve(Host, Port, error);
+    if (error) {
+        std::cerr << "Something went wrong" << std::endl;
+    } else {
         for (const auto& result : results) {
             std::cout << result.endpoint() << std::endl;
         }
     }
+}
+
+TEST(Resolver, AsyncResolve)
+{
+    asio::io_context context;
+    asio::ip::tcp::resolver resolver{context};
+
+    resolver.async_resolve(
+        Host, Port, [](const sys::error_code& error, const tcp::resolver::results_type& results) {
+            if (error) {
+                std::cerr << "Something went wrong" << std::endl;
+            } else {
+                for (const auto& result : results) {
+                    std::cout << result.endpoint() << std::endl;
+                }
+            }
+        });
+
+    context.run();
 }
