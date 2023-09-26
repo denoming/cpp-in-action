2f96e1bb3b36b71a8451b2c36b4fd33a5e6b0d3f
Denys Asauliak <denoming@gmail.com>
2023-09-26 15:39:27 +0300

    move to include dir

--- src/patterns/include/patterns/WrapperFacade.hpp
+++ src/patterns/include/patterns/WrapperFacade.hpp
@@ -6,14 +6,18 @@
  * object-oriented class interfaces.
  *
  * Pros:
- * - Concise, cohesive and robust higher-level object-oriented programming interfaces
- * - Portability and maintainability
- * - Modularity, re-usability and configurability
+ *  - Concise, cohesive and robust higher-level object-oriented programming interfaces
+ *  - Portability and maintainability
+ *  - Modularity, re-usability and configurability
  *
  * Cons:
  *  - Loss of functionality (an 'escape-hatch' mechanism can be used)
  *  - Performance degradation (additional indirection)
  *  - Programming language and compiler limitations (define interfaces across different languages)
+ *
+ * Reference:
+ *   "Pattern-Oriented Software Architecture, Volume 2, Chapter 2"
+ *   By Douglas C. Schmidt, Michael Stal, Hans Rohnert, Frank Buschmann
  */
 class WrapperFacade {
 public:
