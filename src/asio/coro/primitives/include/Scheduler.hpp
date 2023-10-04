9d03224df8521ea261b191b41d8105451bd36252
Denys Asauliak <denoming@gmail.com>
2023-10-04 22:12:01 +0300

    impl scheduler

---
+++ src/asio/coro/primitives/include/Scheduler.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Asio.hpp"
+
+inline io::awaitable<void>
+scheduler(io::any_io_executor executor)
+{
+    return io::async_initiate<decltype(io::use_awaitable), void()>(
+        [executor](io::completion_handler_for<void()> auto&& handler) {
+            io::post(executor,
+                     [handler = std::forward<decltype(handler)>(handler)]() mutable { handler(); });
+        },
+        io::use_awaitable);
+}
