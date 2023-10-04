146762fe7ff581e4c078bebc7d4fc307da8fe295
Denys Asauliak <denoming@gmail.com>
2023-10-04 20:22:32 +0300

    use meaningful value

--- src/asio/coro/primitives/include/SequenceTraits.hpp
+++ src/asio/coro/primitives/include/SequenceTraits.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <type_traits>
+#include <numeric>
 
 template<typename TSequence>
 struct SequenceTraits {
@@ -8,7 +9,7 @@ struct SequenceTraits {
     using difference_type = std::make_signed_t<TSequence>;
     using size_type = std::make_unsigned_t<TSequence>;
 
-    static constexpr value_type initialSequence = static_cast<value_type>(-1);
+    static constexpr value_type initialSequence = std::numeric_limits<value_type>::max();
 
     static constexpr difference_type
     difference(value_type a, value_type b)
