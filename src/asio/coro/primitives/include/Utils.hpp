01edb62e65461f9015e48a03c59a01e165f46982
Denys Asauliak <denoming@gmail.com>
2023-10-09 21:25:39 +0300

    add gen random numbers function

--- src/asio/coro/primitives/include/Utils.hpp
+++ src/asio/coro/primitives/include/Utils.hpp
@@ -3,6 +3,8 @@
 #include "Asio.hpp"
 
 #include <chrono>
+#include <random>
+#include <limits>
 
 template<typename Rep, typename Period>
 io::awaitable<void>
@@ -11,4 +13,12 @@ asyncSleep(std::chrono::duration<Rep, Period> duration)
     auto timer = io::system_timer(co_await io::this_coro::executor);
     timer.expires_after(duration);
     co_await timer.async_wait(io::use_awaitable);
+}
+
+template<typename T = int32_t>
+static T
+generate(T min = std::numeric_limits<T>::min(), T max = std::numeric_limits<T>::max())
+{
+    static std::mt19937 rnd(std::time(nullptr));
+    return std::uniform_int_distribution<>(min, max)(rnd);
 }
